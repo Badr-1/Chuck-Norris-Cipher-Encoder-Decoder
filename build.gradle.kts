fe1e1653b2563ef73edc0cb482c943da4cdbe772
Badr-1 <badrmohamedashraf@gmail.com>
2023-02-26 19:17:39 +0200

    ➕ For Better UI/UX

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,17 @@ repositories {
     mavenCentral()
 }
 
+allprojects {
+    repositories {
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 dependencies {
+    implementation("org.fusesource.jansi:jansi:2.4.0")
+    implementation ("org.jline:jline:3.21.0")
+    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta9")
+    implementation("com.github.kotlin-inquirer:kotlin-inquirer:0.1.0")
     testImplementation(kotlin("test"))
 }
 
