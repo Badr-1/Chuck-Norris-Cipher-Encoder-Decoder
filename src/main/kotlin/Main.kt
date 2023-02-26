c4918da20d9b59a1ed6bac1b9bc4dd2c2e0c280c
Badr-1 <badrmohamedashraf@gmail.com>
2023-02-26 15:56:26 +0200

    ✨ Analyzing the words
    closes #1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
 fun main(args: Array<String>) {
-
+    println("Input string:")
+    val message = readln()
+    val chars = message.toMutableList()
+    chars.forEach { char: Char -> print("$char ") }
 }
