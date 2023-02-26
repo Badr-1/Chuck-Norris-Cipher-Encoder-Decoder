2e1b18652db3b657bf5dbb23f3bb039957a62967
Badr-1 <badrmohamedashraf@gmail.com>
2023-02-26 16:05:07 +0200

    ✨ Converting Words To Binaries
    closes #2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
+const val BIN_LENGTH = 7
+const val PAD_CHAR = '0'
 fun main(args: Array<String>) {
     println("Input string:")
     val message = readln()
     val chars = message.toMutableList()
-    chars.forEach { char: Char -> print("$char ") }
+    val binaries = chars.map { Integer.toBinaryString(it.code) }
+    println("The result:")
+    binaries.forEach {
+        println(
+            "${chars[binaries.indexOf(it)]} = ${it.padStart(BIN_LENGTH, PAD_CHAR)}"
+        )
+    }
 }
