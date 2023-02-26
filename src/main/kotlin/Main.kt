4677daa11c31fa2d4b9125d23db7182f5569fc9b
Badr-1 <badrmohamedashraf@gmail.com>
2023-02-26 16:42:53 +0200

    ✨ Added Encoder
    closes #3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,20 @@ fun main(args: Array<String>) {
     println("Input string:")
     val message = readln()
     val chars = message.toMutableList()
-    val binaries = chars.map { Integer.toBinaryString(it.code) }
+    val binaries = chars.map { Integer.toBinaryString(it.code).padStart(BIN_LENGTH, PAD_CHAR) }
+    val joinedBinaries = binaries.joinToString(separator = "")
+    val encryptedBinary = mutableListOf<String>()
+    var binary = joinedBinaries
+    while (binary.isNotEmpty()) {
+        val bin = binary.substringBefore(if (binary[0] == '0') '1' else '0')
+        binary = binary.substringAfter(bin)
+        // encrypt
+        encryptedBinary.add(if (bin[0] == '0') "00" else "0")
+        encryptedBinary.add("0".repeat(bin.length))
+    }
+
     println("The result:")
-    binaries.forEach {
-        println(
-            "${chars[binaries.indexOf(it)]} = ${it.padStart(BIN_LENGTH, PAD_CHAR)}"
-        )
+    encryptedBinary.forEach {
+        print("$it ")
     }
 }
